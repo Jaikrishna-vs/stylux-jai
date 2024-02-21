4d56088df048dea0a63f9df1b9d77cf306a96908
Jaikrishna-vs <154946003+Jaikrishna-vs@users.noreply.github.com>
2024-02-21 22:08:56 +0530

    Add files via upload

---
+++ Stylux/script.js
@@ -0,0 +1,13 @@
+var a=document.querySelector(".side-nav-bar")
+function popup(){
+    a.style.left="0%"
+
+}
+
+var b=document.querySelector(".side-nav-bar")
+function popdown(){
+    a.style.left="-50%"
+}
+
+
+
